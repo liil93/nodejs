2a5cf4756d92b4e45569e6f450a3e720f01db857
liil93 <liil93@github.com>
2017-05-12 13:03:26 +0900

    170512

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var temp1 = require('./routes/temp1');
+var temp2 = require('./routes/temp2');
+
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/temp1', temp1);
+app.use('/temp2', temp2);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -4,3 +4,30 @@ spinner = document.querySelector("#spinner");
 if (!sign) { angle = angle + 36; } else { angle = angle - 36; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
+
+$('#carouselHacked').carousel();
+
+// mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+$('.owl-carousel').owlCarousel({
+loop: true,
+margin: 10,
+nav: true,
+navText: [
+	"<i class='fa fa-caret-left'></i>",
+	"<i class='fa fa-caret-right'></i>"
+],
+autoplay: true,
+autoplayHoverPause: true,
+responsive: {
+	0: {
+		items: 1
+	},
+	600: {
+		items: 3
+	},
+	1000: {
+		items: 5
+	}
+}
+})
